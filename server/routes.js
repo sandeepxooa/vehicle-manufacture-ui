dbf54fee5e87dd3b0a142cc8f3a3909e9555d5e7
Sandeep Patel <sandeep.patel@alepo.com>
2018-07-11 18:49:05 +0530

    Commit

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
   'use strict'
 
   app.use('/orders', require(__dirname+'/../node_modules/vehicle-manufacture-manufacturing/server/api/orders'))
+  app.use('/placeorder', require(__dirname+'/../node_modules/vehicle-manufacture-manufacturing/server/api/placeorder'))
   app.use('/updateOrderStatus', require(__dirname+'/../node_modules/vehicle-manufacture-manufacturing/server/api/updateOrderStatus'))
   app.use('/transactions', require(__dirname+'/../node_modules/vehicle-manufacture-vda/server/api/transactions'))
   app.use('/vehicles', require(__dirname+'/../node_modules/vehicle-manufacture-vda/server/api/vehicles'))
